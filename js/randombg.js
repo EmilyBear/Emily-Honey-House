a198f7ab083060abdbd9028ffbe72cd1a699956f
DeployBear <autodeploy@emiliabear.com>
2022-09-19 22:18:17 +0800

    GitHub Actions Auto Builder at 2022-09-19 22:18:17

--- js/randombg.js
+++ js/randombg.js
@@ -1,10 +1,1 @@
-// 随机背景
-document.getElementById(web_bg).style.backgroundImage = backimg[bgindex];
-var bannerimg =[
-  url(imgbg1.JPG),
-  url(imgbg2.jpg),
-  url(imgbg3.jpg),
-  url(imgbg4.jpg)
-];
-var bannerindex =Math.ceil(Math.random()  (bannerimg.length-1));
-document.getElementById(page-header).style.backgroundImage = bannerimg[bannerindex];
+document.getElementById(web_bg).style.backgroundImage=backimg[bgindex];var bannerimg=[url(imgbg1.JPG),url(imgbg2.jpg),url(imgbg3.jpg),url(imgbg4.jpg)],bannerindex=Math.ceil(Math.random()(bannerimg.length-1));document.getElementById(page-header).style.backgroundImage=bannerimg[bannerindex];
